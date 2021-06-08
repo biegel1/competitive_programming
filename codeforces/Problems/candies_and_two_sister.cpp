369ea2b9503f2d9775420689dc2538d3abdc6d9c
Tobias Biegel <tobias.biegel@tu-darmstadt.de>
2021-06-08 14:24:43 +0200

    adds new problem

---
+++ codeforces/Problems/candies_and_two_sister.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+void solve(){
+	int n; 
+	cin >> n; 
+	if(n%2 == 0){
+		cout << n/2 -1 << endl;
+	}
+	else{
+		cout << n/2 << endl;
+	}
+}
+
+
+int main(){
+	int t; 
+	cin >> t; 
+	while(t--){
+		solve();
+	}
+
+	return 0;
+}
