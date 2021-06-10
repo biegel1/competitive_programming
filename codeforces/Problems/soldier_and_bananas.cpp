f1e865fb7dd84ee06508a32cc816f17f21364113
Tobias Biegel <tobias.biegel@tu-darmstadt.de>
2021-06-10 17:58:58 +0200

    adds solution to problem

--- codeforces/Problems/soldier_and_bananas.cpp
+++ codeforces/Problems/soldier_and_bananas.cpp
@@ -5,6 +5,18 @@ using namespace std;
 
 void solve(){
 
+	int k, n, w; 
+	cin >> k>> n>>w; 
+	int m; 
+	m = k*(w*(w+1)/2)-n;
+	if(m>0){
+		cout << m << endl; 
+	}
+	else{
+		cout << 0 << endl; 
+	}
+	
+
 
 }
 
